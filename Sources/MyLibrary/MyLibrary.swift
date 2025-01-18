ff5c42d9e15acc6cf04630533bf616431870053f
ex_yujh11 <ex_yujh11@partner.midea.com>
2025-01-18 17:26:00 +0800

    add function

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,2 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public class MyClass {
+    public init() {} // 添加 public 初始化器
+
+    public func my_func() {
+        print("helloworld")
+    }
+}
